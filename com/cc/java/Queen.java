eafa187adc085009f8870cf918d21d38dbe949e4
Nekroid <philipleinung@aol.de>
2024-01-31 09:25:35 +0100

    doYourWork ++

--- com/cc/java/Queen.java
+++ com/cc/java/Queen.java
@@ -2,5 +2,8 @@ package com.cc.java;
 
 public class Queen extends HoneyBee
 {
-    
+    public String doYourJob()
+    {
+        return "I'm the Queen. Beware Human, don't underress my power!";
+    }
 }
--- com/cc/java/Worker.java
+++ com/cc/java/Worker.java
@@ -2,5 +2,8 @@ package com.cc.java;
 
 public class Worker extends HoneyBee
 {
-    
+    public String doYourJob()
+    {
+        return "I'm a worker bee. I work, work, work, work";
+    }
 }
