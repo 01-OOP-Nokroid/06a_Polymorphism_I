f4c726be3aa5b2ea3bf6cbe385e61249de6d58a8
Nekroid <philipleinung@aol.de>
2024-01-31 10:54:19 +0100

    Ergänzung

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -14,7 +14,13 @@ public class App
         pollObj(queen);
         pollObj(worker);
         pollObj(drone);
+
+        // Das sollte eigentlich nicht gehen! //
+        HoneyBee bee = new HoneyBee();
+        pollObj(bee);
     }
+
+
     // 3. Iteration Polymorphie // 
     private static void pollObj(HoneyBee obj)
     {
